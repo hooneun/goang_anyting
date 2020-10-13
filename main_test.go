132c9c6bb631440b71a95e83c4c1f3746980c129
Hoon <kimhoon0316@gmail.com>
2020-10-13 13:40:42 +0900

    GetUserByID Test code

--- main_test.go
+++ main_test.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
+
+	"github.com/hooneun/golang_anyting/model"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -14,6 +18,20 @@ func TestPingRoute(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/ping", nil)
 	router.ServeHTTP(w, req)
 
-	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, http.StatusOK, w.Code)
 	assert.JSONEq(t, `{"message":"pong"}`, w.Body.String())
+}
+
+func TestGetUserByIdRoute(t *testing.T) {
+	router := setupRouter()
+	w := httptest.NewRecorder()
+	id := 1
+	req, _ := http.NewRequest("GET", "/user/"+strconv.Itoa(id), nil)
+	router.ServeHTTP(w, req)
+
+	user, _ := model.GetUserByID(id)
+	jsonUser, _ := json.Marshal(user)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.JSONEq(t, string(jsonUser), w.Body.String())
 }
