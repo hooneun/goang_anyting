a1de70ea860ec1d1cd61a68cd8861418853b5c90
김훈 <hooneun@kimhoon-ui-MacBookPro.local>
2020-10-19 20:41:06 +0900

    goroutine

--- goroutines.go
+++ goroutines.go
@@ -104,28 +104,99 @@ func main() {
 	// Timeouts
 	// https://gobyexample.com/timeouts
 
-	c1 := make(chan string, 1)
-	go func() {
-		time.Sleep(2 * time.Second)
-		c1 <- "result 1"
-	}()
+	// c1 := make(chan string, 1)
+	// go func() {
+	// 	time.Sleep(2 * time.Second)
+	// 	c1 <- "result 1"
+	// }()
 
-	select {
-	case res := <-c1:
-		fmt.Println(res)
-	case <-time.After(1 * time.Second):
-		fmt.Println("timeout 1")
-	}
+	// select {
+	// case res := <-c1:
+	// 	fmt.Println(res)
+	// case <-time.After(1 * time.Second):
+	// 	fmt.Println("timeout 1")
+	// }
 
-	c2 := make(chan string, 1)
-	go func() {
-		time.Sleep(2 * time.Second)
-		c2 <- "result 2"
-	}()
-	select {
-	case res := <-c2:
-		fmt.Println(res)
-	case <-time.After(3 * time.Second):
-		fmt.Println("timeout 2")
+	// c2 := make(chan string, 1)
+	// go func() {
+	// 	time.Sleep(2 * time.Second)
+	// 	c2 <- "result 2"
+	// }()
+	// select {
+	// case res := <-c2:
+	// 	fmt.Println(res)
+	// case <-time.After(3 * time.Second):
+	// 	fmt.Println("timeout 2")
+	// }
+
+	// Non-blocking channel Operations
+	// https://gobyexample.com/non-blocking-channel-operations
+
+	// messages := make(chan string)
+	// signals := make(chan bool)
+
+	// select {
+	// case msg := <-messages:
+	// 	fmt.Println("received message", msg)
+	// default:
+	// 	fmt.Println("no message received")
+	// }
+
+	// msg := "hi"
+	// select {
+	// case messages <- msg:
+	// 	fmt.Println("sent message", msg)
+	// default:
+	// 	fmt.Println("no message sent")
+	// }
+
+	// select {
+	// case msg := <-messages:
+	// 	fmt.Println("received message", msg)
+	// case sig := <-signals:
+	// 	fmt.Println("received signal", sig)
+	// default:
+	// 	fmt.Println("no activity")
+	// }
+
+	// Closing Channels
+	// https://gobyexample.com/closing-channels
+
+	// jobs := make(chan int, 5)
+	// done := make(chan bool)
+
+	// go func() {
+	// 	fmt.Println("func()")
+	// 	for {
+	// 		j, more := <-jobs
+	// 		if more {
+	// 			fmt.Println("recevied job", j)
+	// 		} else {
+	// 			fmt.Println("received all jobs")
+	// 			done <- true
+	// 			return
+	// 		}
+	// 	}
+	// }()
+
+	// for j := 1; j <= 3; j++ {
+	// 	jobs <- j
+	// 	fmt.Println("sent job", j)
+	// }
+	// close(jobs)
+	// fmt.Println("sent all jobs")
+
+	// <-done
+
+	// Range over Channels
+	// https://gobyexample.com/range-over-channels
+
+	queue := make(chan string, 2)
+	queue <- "one"
+	queue <- "two"
+	close(queue)
+
+	for elem := range queue {
+		fmt.Println(elem)
 	}
 }
