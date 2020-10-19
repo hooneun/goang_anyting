231db7649c77bc46b2f649aa01a7fcf66fd20661
Hoon <kimhoon0316@gmail.com>
2020-10-19 18:17:09 +0900

    Timeouts

--- goroutines.go
+++ goroutines.go
@@ -79,25 +79,53 @@ func main() {
 	// Select!!!
 	// https://gobyexample.com/select
 
-	c1 := make(chan string)
-	c2 := make(chan string)
+	// c1 := make(chan string)
+	// c2 := make(chan string)
 
+	// go func() {
+	// 	time.Sleep(2 * time.Second)
+	// 	c1 <- "one"
+	// }()
+
+	// go func() {
+	// 	time.Sleep(1 * time.Second)
+	// 	c2 <- "two"
+	// }()
+
+	// for i := 0; i < 2; i++ {
+	// 	select {
+	// 	case msg1 := <-c1:
+	// 		fmt.Println("received", msg1)
+	// 	case msg2 := <-c2:
+	// 		fmt.Println("received", msg2)
+	// 	}
+	// }
+
+	// Timeouts
+	// https://gobyexample.com/timeouts
+
+	c1 := make(chan string, 1)
 	go func() {
 		time.Sleep(2 * time.Second)
-		c1 <- "one"
+		c1 <- "result 1"
 	}()
 
+	select {
+	case res := <-c1:
+		fmt.Println(res)
+	case <-time.After(1 * time.Second):
+		fmt.Println("timeout 1")
+	}
+
+	c2 := make(chan string, 1)
 	go func() {
-		time.Sleep(1 * time.Second)
-		c2 <- "two"
+		time.Sleep(2 * time.Second)
+		c2 <- "result 2"
 	}()
-
-	for i := 0; i < 2; i++ {
-		select {
-		case msg1 := <-c1:
-			fmt.Println("received", msg1)
-		case msg2 := <-c2:
-			fmt.Println("received", msg2)
-		}
+	select {
+	case res := <-c2:
+		fmt.Println(res)
+	case <-time.After(3 * time.Second):
+		fmt.Println("timeout 2")
 	}
 }
